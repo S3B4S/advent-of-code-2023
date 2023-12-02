89c055ac226d6305230954691357bd5591117076
Kevin van der Werff <kevin.vanderwerff@deptagency.com>
2023-12-02 21:12:03 +0100

    feat(main): remove duplicate test case

--- 02_cube-conundrum/index.test.ts
+++ 02_cube-conundrum/index.test.ts
@@ -15,16 +15,6 @@ testWrapper(`Day 02`, () => {
   test("Part 1 - Example input", () => {
     expect(solvePart1(exampleInput)).toEqual(8)
   })
-
-  test("Part 1 - Example input 2", () => {
-    expect(solvePart1(`
-Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
-    `.trim())).toEqual(8)
-  })
   
   test("Part 1 - File input", () => {
     expect(solvePart1(fileInput)).toEqual(3059)
