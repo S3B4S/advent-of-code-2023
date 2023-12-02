1debc48c07e1573cbdbdd00f526927f698f2bf01
Kevin van der Werff <kevin.vanderwerff@deptagency.com>
2023-12-02 06:26:21 +0100

    feat(main): part 2 day 2

--- 02_challenge/index.test.ts
+++ 02_challenge/index.test.ts
@@ -16,17 +16,17 @@ testWrapper(`Day 02`, () => {
     expect(solvePart1(exampleInput)).toEqual(8)
   })
 
-  test.skip("Part 1 - Example input 2", () => {
+  test("Part 1 - Example input 2", () => {
     expect(solvePart1(`
-    Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
     `.trim())).toEqual(8)
   })
   
-  test.skip("Part 1 - File input", () => {
+  test("Part 1 - File input", () => {
     expect(solvePart1(fileInput)).toEqual(3059)
   })
 
@@ -35,6 +35,6 @@ testWrapper(`Day 02`, () => {
   })
   
   test("Part 2 - File input", () => {
-    expect(solvePart2(fileInput)).toEqual(0)
+    expect(solvePart2(fileInput)).toEqual(65371)
   })
 })
--- 02_challenge/index.ts
+++ 02_challenge/index.ts
@@ -19,7 +19,7 @@ export const solvePart1 = (input: string) => {
     const [_, id, ...cubes] = line.split(' ')
     const realId = Number(id.slice(0, -1))
     const sets = cubes.join(' ').split('; ').map(x => x.split(', '))
-    console.log(realId)
+    
     const isPossible = sets.every(set => {
       return set.every(cubes => {
         const [amount, color] = cubes.split(' ')
@@ -39,16 +39,44 @@ export const solvePart1 = (input: string) => {
     }
     return
   })
-  
-  // const y = x.filter(({ red, green, blue }) => {
-  //   return red <= maximum.red && green <= maximum.green && blue <= maximum.blue
-  // }).reduce((acc, curr) => acc + curr.id, 0)
 
   return ids.reduce((acc, curr) => acc + curr, 0)
 }
 
 export const solvePart2 = (input: string) => {
-  return 0
+  const lines = input.split('\n')
+  const ids = []
+  const x = lines.map(line => {
+    const [_, id, ...cubes] = line.split(' ')
+    const realId = Number(id.slice(0, -1))
+    const sets = cubes.join(' ').split('; ').map(x => x.split(', '))
+    // console.log(realId)
+    let maxRed = 0
+    let maxGreen = 0
+    let maxBlue = 0
+
+    const isPossible = sets.forEach(set => {
+      return set.forEach(cubes => {
+        const [amount, color] = cubes.split(' ')
+        if (color.includes('red')) {
+          maxRed = Math.max(maxRed, Number(amount))
+        } else if (color.includes('green')) {
+          maxGreen = Math.max(maxGreen, Number(amount))
+        } else if (color.includes('blue')) {
+          maxBlue = Math.max(maxBlue, Number(amount))
+        }
+        return false
+      })
+    })
+
+    return maxRed * maxGreen * maxBlue
+  })
+  
+  // const y = x.filter(({ red, green, blue }) => {
+  //   return red <= maximum.red && green <= maximum.green && blue <= maximum.blue
+  // }).reduce((acc, curr) => acc + curr.id, 0)
+
+  return x.reduce((acc, curr) => acc + curr, 0)
 }
 
 // 289 NOT RIGHT
