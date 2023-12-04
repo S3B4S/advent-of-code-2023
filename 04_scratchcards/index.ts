6c988806ac7d7a5b9f3d7bfc40bc18473958b30e
Kevin van der Werff <kevin.vanderwerff@deptagency.com>
2023-12-04 06:34:53 +0100

    feat(main): slight clean up day 4

--- 04_scratchcards/index.ts
+++ 04_scratchcards/index.ts
@@ -1,4 +1,3 @@
-import { logId } from "@/utils/misc"
 import { parseInputLines } from "@/utils/parsing"
 
 export const solvePart1 = (input: string) => {
@@ -13,7 +12,6 @@ export const solvePart1 = (input: string) => {
     return intersection.reduce((acc, curr) => (acc === 0) ? 1 : acc * 2, 0)
   })
 
-
   return res.reduce((acc, curr) => acc + curr, 0)
 }
 
@@ -26,15 +24,13 @@ export const solvePart2 = (input: string) => {
 
   let amount = [] as number[]
   chunks.forEach((_, index) => amount[index] = 1)
-  const res = chunks.map(({ startNumbers, end, id }) => {
+  chunks.forEach(({ startNumbers, end, id }) => {
     const intersection = [...startNumbers].filter(x => end.includes(x)).filter(x => x !== '')
     for (let i = Number(id); i < Number(id) + intersection.length; i++) {
       const amountCurrentCard = amount[Number(id) - 1] || 1
       amount[Number(i)] = (amount[Number(i)] || 1) + amountCurrentCard
     }
-    return intersection.length
   })
-
 
   return amount.filter(x => !isNaN(x)).reduce((acc, curr) => acc + curr, 0)
 }
