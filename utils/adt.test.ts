11b033e0e17262d07a0e878c06a86b3afd18e6f0
Kevin van der Werff <kevin.vanderwerff@deptagency.com>
2023-12-06 23:09:36 +0100

    feat(main): add queue

--- utils/adt.test.ts
+++ utils/adt.test.ts
@@ -1,8 +1,12 @@
 import { expect, test } from 'bun:test'
-import { Stack } from './adt'
+import { Stack, Queue } from './adt'
 
-test('Stack test', () => {
+test('Stack - Push 1', () => {
   const stack = new Stack()
+  expect(stack.size()).toEqual(0)
+  expect(stack.isEmpty()).toEqual(true)
+  expect(stack.peek()).toEqual(undefined)
+  expect(stack.pop()).toEqual(undefined)
   expect(stack.isEmpty()).toEqual(true)
   stack.push(1)
   expect(stack.isEmpty()).toEqual(false)
@@ -10,4 +14,48 @@ test('Stack test', () => {
   expect(stack.peek()).toEqual(1)
   expect(stack.pop()).toEqual(1)
   expect(stack.isEmpty()).toEqual(true)
+})
+
+test("Stack - Push multiple", () => {
+  const stack = new Stack()
+  stack.push(1)
+  stack.push(2)
+  stack.push(3)
+  expect(stack.size()).toEqual(3)
+  expect(stack.peek()).toEqual(3)
+  expect(stack.pop()).toEqual(3)
+  expect(stack.pop()).toEqual(2)
+  expect(stack.pop()).toEqual(1)
+  expect(stack.pop()).toEqual(undefined)
+  expect(stack.isEmpty()).toEqual(true)
+})
+
+test("Queue - Enqueue 1", () => {
+  const queue = new Queue()
+  expect(queue.size()).toEqual(0)
+  expect(queue.isEmpty()).toEqual(true)
+  expect(queue.peek()).toEqual(undefined)
+  expect(queue.dequeue()).toEqual(undefined)
+  expect(queue.isEmpty()).toEqual(true)
+  queue.enqueue(1)
+  expect(queue.isEmpty()).toEqual(false)
+  expect(queue.size()).toEqual(1)
+  expect(queue.peek()).toEqual(1)
+  expect(queue.dequeue()).toEqual(1)
+  expect(queue.isEmpty()).toEqual(true)
+})
+
+test("Queue - Enqueue multiple", () => {
+  const queue = new Queue()
+  queue.enqueue(1)
+  queue.enqueue(2)
+  queue.enqueue(3)
+  expect(queue.size()).toEqual(3)
+  expect(queue.peek()).toEqual(1)
+  expect(queue.dequeue()).toEqual(1)
+  expect(queue.size()).toEqual(2)
+  expect(queue.dequeue()).toEqual(2)
+  expect(queue.size()).toEqual(1)
+  expect(queue.dequeue()).toEqual(3)
+  expect(queue.size()).toEqual(0)
 })
--- utils/adt.ts
+++ utils/adt.ts
@@ -23,3 +23,29 @@ export class Stack {
     console.log(this._store);
   }
 }
+
+export class Queue {
+  private _store: any[] = [];
+  constructor() {}
+  enqueue(item: any) {
+    this._store.push(item);
+  }
+  dequeue() {
+    return this._store.shift();
+  }
+  peek() {
+    return this._store[0];
+  }
+  isEmpty() {
+    return this._store.length === 0;
+  }
+  size() {
+    return this._store.length;
+  }
+  clear() {
+    this._store = [];
+  }
+  print() {
+    console.log(this._store);
+  }
+}
