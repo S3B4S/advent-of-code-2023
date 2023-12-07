9e487872e0d04c32000b56c0e6080760a1f47e73
Kevin van der Werff <kevin.vanderwerff@deptagency.com>
2023-12-07 10:42:28 +0100

    feat(main): day 5 part 2 cleanup

--- 05_if-you-give-a-seed-a-fertilizer/index.ts
+++ 05_if-you-give-a-seed-a-fertilizer/index.ts
@@ -3,13 +3,10 @@ import { parseInputBlocks, parseInputLines } from "@/utils/parsing"
 
 export const solvePart1 = (input: string) => {
   const [rawSeeds, ...maps] = parseInputBlocks(input)
-  const [seedToSoilMap, soilToFertMap, fertToWaterMap, waterToLightMap, lightToTempMap, tempToHumMap, humToLocationMap] = maps
   const seeds = rawSeeds[0].split(':')[1].trim().split(' ').map(x => Number(x))
 
   maps.forEach(map => {
     map.shift()
-    // console.log('====')
-    // console.log(map)
     const isTransported = seeds.map(_ => false) // Keep track of seeds that have already been transported
     map.forEach(setOfNumbers => {
       const [destRangeStart, sourceRangeStart, rangeLength] = setOfNumbers.split(' ').map(Number)
@@ -17,21 +14,15 @@ export const solvePart1 = (input: string) => {
         if (seed <= sourceRangeStart + rangeLength && seed >= sourceRangeStart && !isTransported[index]) {
           const diff = seed - sourceRangeStart
           const dest = destRangeStart + diff
-          // console.log(seeds)
-          // console.log(isTransported)
           seeds[index] = dest
           isTransported[index] = true
-          // console.log(seed, dest)
         } else {
           const dest = seed
           seeds[index] = dest
-          // console.log(seed, dest)
         }
       })
     })
-    // console.log(seeds)
   })
-  // console.log(seeds)
 
   return seeds.reduce((acc, seed) => Math.min(acc, seed), Infinity)
 }
@@ -52,34 +43,14 @@ type Interval = [number, number]
 type PointWithRange = [number, number]
 
 export const solvePart2 = (input: string) => {
-  // const newInput = "seeds: " + logId(chunksOf2(input.split('\n')[0].split(':')[1].trim().split(' ')).map(pair => {
-  //   const [rangeStart, rangeLength] = pair.map(Number)
-  //   const newRange = []
-  //   for (let i = 0; i < rangeLength; i++) {
-  //     newRange.push(rangeStart + i)
-  //   }
-  //   return newRange
-  // }).flat().join(' '))
-
-  // const [rawSeeds, ...maps] = parseInputLines(input)
-  // // console.log(maps)
-  // const newInputX = newInput + '\n' + maps.join('\n')
-  // // console.log(newInputX)
-
-
-  // return solvePart1(newInputX)
-
   const [rawSeeds, ...maps] = parseInputBlocks(input)
   let seedRanges = chunksOf2(rawSeeds[0].split(':')[1].trim().split(' ').map(x => Number(x)))
 
   maps.forEach(map => {
     map.shift()
     const seedsNewMap = [] as [number, number][]
-    // console.log('===NEW MAP==')
-    // console.log(seedRanges)
     const hasBeenTransformed = seedRanges.map(_ => false)
     map.forEach(setOfNumbers => {
-      // console.log('===NEW SET OF NUMBERS WITHIN MAP==')
       const [mapDestRangeStart, mapSourceRangeStart, mapRangeLength] = setOfNumbers.split(' ').map(Number)
       const mapSourceRangeEnd = mapSourceRangeStart + mapRangeLength - 1
 
@@ -90,7 +61,6 @@ export const solvePart2 = (input: string) => {
         const end = start + range - 1
 
         if (mapSourceRangeStart <= start && mapSourceRangeEnd >= end) {
-          // console.log('||| Case 4 ')
           // Now the entire seedRange has been captured, so everything moves
           const delta = start - mapSourceRangeStart
           
@@ -98,7 +68,6 @@ export const solvePart2 = (input: string) => {
           seedsNewMap.push(newRange)
           hasBeenTransformed[index] = true
         } else if (start < mapSourceRangeStart && mapSourceRangeStart <= end && end < mapSourceRangeEnd) {
-          // console.log('||| Case 1 ')
           // To split at mapSourceRangeStart
           // start -> mapSourceRangeStart stays the same
           // mapSourceRangeStart -> end will move
@@ -111,7 +80,6 @@ export const solvePart2 = (input: string) => {
           hasBeenTransformed.push(false)
           hasBeenTransformed[index] = true
         } else if (mapSourceRangeStart < start && start <= mapSourceRangeEnd && mapSourceRangeEnd < end) {
-          // console.log('||| Case 2 ')
           // start -> mapSourceRangeEnd stays the same
           // mapSourceRangeEnd -> end will be moved
           const moves: Interval = [start, mapSourceRangeEnd]
@@ -124,7 +92,6 @@ export const solvePart2 = (input: string) => {
           hasBeenTransformed.push(false)
           hasBeenTransformed[index] = true
         } else if (mapSourceRangeEnd <= end && mapSourceRangeStart >= start) {
-          // console.log('||| Case 3 ')
           // Now it might need to split in 3 parts
           // start -> mapSourceRangeStart stays the same
           // mapSourceRangeStart -> mapSourceRangeEnd will move
@@ -147,7 +114,6 @@ export const solvePart2 = (input: string) => {
           }
 
         } else {
-          // console.log('||| Case 5 ')
           // The two intervals are entirely separate
           // So there's no moving needed
         }
