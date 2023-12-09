8d76c415da0068c5f2d3380784d2f5a63533699a
Kevin van der Werff <kevin.vanderwerff@deptagency.com>
2023-12-09 12:23:05 +0100

    feat(main): cleanup day 9 + fix regex

--- 09_mirage-maintenance/index.ts
+++ 09_mirage-maintenance/index.ts
@@ -1,7 +1,7 @@
 import { logId } from "@/utils/misc"
 import { parseInputLines } from "@/utils/parsing"
 
-const calculateHistory = (sequence: number[]) => {
+const calculateHistory = (sequence: number[], shouldPrepend: boolean = false) => {
   const sequences = [sequence]
   
   // Go "downwards" finding all the histories
@@ -27,7 +27,7 @@ const calculateHistory = (sequence: number[]) => {
   
   while(i--) {
     const currentSeq = sequences[i]
-    const newNumber = currentSeq.at(-1)! + numberToAdd
+    const newNumber = shouldPrepend ? currentSeq.at(0)! - numberToAdd : currentSeq.at(-1)! + numberToAdd
     numberToAdd = newNumber
   }
 
@@ -36,55 +36,16 @@ const calculateHistory = (sequence: number[]) => {
 
 export const solvePart1 = (input: string) => {
   const sequences = parseInputLines(input)
-    .map(line => line.split(' ').map(x => Number(x)))
-    // INVESTIGATE WHY line.split(/\W+/) did not work
-    // .map(line => line.split(/\W+/).map(x => Number(x)))
+    .map(line => line.split(/[^\w-]/).map(x => Number(x)))
   
-  const results = sequences.map(calculateHistory)
+  const results = sequences.map(seq => calculateHistory(seq))
   return results.reduce((acc, curr) => acc + curr)
 }
 
-
-const calculateHistoryReversed = (sequence: number[]) => {
-  const sequences = [sequence]
-  
-  // Go "downwards" finding all the histories
-  while(true) {
-    const currentSeq = sequences.at(-1)!
-    if (currentSeq.every(x => x === 0)) break
-    const newSeq = currentSeq.reduce((acc, curr, index, list) => {
-      // Skip the first number, as we can't look back to compare to anything
-      if (index === 0) return []
-      const prev = list[index - 1]
-      const diff = curr - prev
-      return [...acc, diff]
-    }, [] as number[])
-    sequences.push(newSeq)
-  }
-
-  // Bubble back upwards, to find the lost history number
-  // The one to last will be an array of all equal numbers,
-  // add this number to the second to last array, and then
-  // bubble up
-  let numberToAdd = sequences.at(-2)![0]
-  let i = sequences.length - 2
-  
-  while(i--) {
-    const currentSeq = sequences[i]
-    const newNumber = currentSeq.at(0)! - numberToAdd
-    numberToAdd = newNumber
-  }
-
-  return numberToAdd
-}
-
-
 export const solvePart2 = (input: string) => {
   const sequences = parseInputLines(input)
-    .map(line => line.split(' ').map(x => Number(x)))
-    // INVESTIGATE WHY line.split(/\W+/) did not work
-    // .map(line => line.split(/\W+/).map(x => Number(x)))
+    .map(line => line.split(/[^\w-]/).map(x => Number(x)))
   
-  const results = sequences.map(calculateHistoryReversed)
+  const results = sequences.map(seq => calculateHistory(seq, true))
   return results.reduce((acc, curr) => acc + curr)
 }
