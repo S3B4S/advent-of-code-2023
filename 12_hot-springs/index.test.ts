623fb44b99b1c6b535d94b7aeaabaf0353b08e89
Kevin van der Werff <kevin.vanderwerff@deptagency.com>
2023-12-13 00:28:57 +0100

    feat(main): solve day 12

--- 12_hot-springs/index.test.ts
+++ 12_hot-springs/index.test.ts
@@ -14,18 +14,88 @@ const exampleInput = `
 
 testWrapper(`Day 12`, () => {
   test("Part 1 - Example input", () => {
-    expect(solvePart1(exampleInput)).toEqual(21)
+    // Using the implementation of part 2, because it's more optimised
+    expect(solvePart2(exampleInput)).toEqual(21)
   })
   
   test("Part 1 - File input", () => {
-    expect(solvePart1(fileInput)).toEqual(7251)
+    // Using the implementation of part 2, because it's more optimised
+    expect(solvePart2(fileInput)).toEqual(7251)
   })
 
-  test.skip("Part 2 - Example input", () => {
-    expect(solvePart2(exampleInput)).toEqual(0)
+  // Example input for part 2 WITHOUT unfolding
+  test("Part 2 - Example input without unfolding", () => {
+    expect(solvePart2(exampleInput)).toEqual(21)
+  })
+
+  test("Part 2 - Example input line 1 without unfolding", () => {
+    expect(solvePart2(`???.### 1,1,3`)).toEqual(1)
+  })
+
+  test("Part 2 - Example input line 2 without unfolding", () => {
+    expect(solvePart2(`.??..??...?##. 1,1,3`)).toEqual(4)
+  })
+
+  test("Part 2 - Example input line 3 without unfolding", () => {
+    expect(solvePart2(`?#?#?#?#?#?#?#? 1,3,1,6`)).toEqual(1)
   })
   
-  test.skip("Part 2 - File input", () => {
-    expect(solvePart2(fileInput)).toEqual(0)
+  test("Part 2 - Example input line 3 - isolated edge case - attempt 1", () => {
+    expect(solvePart2(`?#?#???#? 1,3,1`)).toEqual(1)
+  })
+  
+  test("Part 2 - Example input line 3 - isolated edge case - attempt 2", () => {
+    expect(solvePart2(`?#?#???#?#?#?#? 1,3,1,6`)).toEqual(1)
+  })
+  
+  test("Part 2 - Example input line 4 without unfolding", () => {
+    expect(solvePart2(`????.######..#####. 1,6,5`)).toEqual(4)
+  })
+
+  test("Part 2 - Example input line 5 without unfolding", () => {
+    expect(solvePart2(`?###???????? 3,2,1`)).toEqual(10)
+  })
+
+  // Example input for part 2 WITH unfolding
+  test("Part 2 - Example input with unfolding", () => {
+    expect(solvePart2(exampleInput, true)).toEqual(525152)
+  })
+
+  test("Part 2 - Example input line 1 with unfolding", () => {
+    expect(solvePart2(`???.### 1,1,3`, true)).toEqual(1)
+  })
+
+  test("Part 2 - Example input line 2 with unfolding", () => {
+    expect(solvePart2(`.??..??...?##. 1,1,3`, true)).toEqual(16384)
+  })
+
+  test("Part 2 - Example input line 3 - unfolded twice", () => {
+    expect(solvePart2(`?#?#?#?#?#?#?#???#?#?#?#?#?#?#? 1,3,1,6,1,3,1,6`)).toEqual(1)
+  })
+
+  test("Part 2 - Example input line 3 with unfolding", () => {
+    expect(solvePart2(`?#?#?#?#?#?#?#? 1,3,1,6`, true)).toEqual(1)
+  })
+
+  // Others
+  test("Part 2 - Self made line", () => {
+    expect(solvePart2(`????#...#....?? 3,1,2`)).toEqual(1)
+  })
+
+  test("Part 2 - Self made line 2", () => {
+    expect(solvePart2(`.#....?? 1,2`)).toEqual(1)
+  })
+
+  test("Part 2 - Empty input", () => {
+    expect(solvePart2(`............... `)).toEqual(1)
+  })
+
+  test("Part 2 - Empty input", () => {
+    expect(solvePart2(`.???..??.??..?..?.. `)).toEqual(1)
+  })
+  
+  // File input
+  test("Part 2 - File input", () => {
+    expect(solvePart2(fileInput, true)).toEqual(2128386729962)
   })
 })
--- 12_hot-springs/index.ts
+++ 12_hot-springs/index.ts
@@ -1,6 +1,12 @@
 import { logId } from "@/utils/misc"
 import { parseInputLines } from "@/utils/parsing"
 
+const springCharacter = {
+  operational: ".",
+  damaged: "#",
+  unknown: "?"
+}
+
 const countStr = (str: string, substr: string) => str.split(substr).length - 1
 
 /**
@@ -26,7 +32,7 @@ export const solvePart1 = (input: string) => {
     .map(line => {
       const [springs, ...groupsSesrialised] = line.split(/[ ,]/)
       const groups = groupsSesrialised.map(g => Number(g))
-      // These are al; derived values which usually we would
+      // These are all derived values which usually we would
       // not want to store
       const nUnknown = countStr(springs, "?")
       const nFunctional = countStr(springs, "#")
@@ -68,6 +74,126 @@ export const solvePart1 = (input: string) => {
   return count
 }
 
-export const solvePart2 = (input: string) => {
-  return 0
+// A record where the keys are the sequence strings + the groups and the values are the possible number of combinations
+const cache = {} as Record<string, number>
+
+const saveToCache = (seq: string, combinations: number[], value: number) => {
+  const key = seq + '|' + combinations.join(",")
+  cache[key] = value
+}
+
+const readFromCache = (seq: string, combinations: number[]) => {
+  const key = seq + '|' + combinations.join(",")
+  return cache[key]
+}
+
+/**
+ * Slides a window over string or array. For example, if n is 3 and input is "hello",
+ * the function will return an array of substrings of length 3: ["hel", "ell", "llo"].
+ * @param n the window size
+ * @returns all scanned items
+ */
+export const sliding = (n: number) => (input: string): string[] => {
+  if (input.length <= n) return [input]
+  return [input.slice(0, n)].concat(sliding(n)(input.slice(1)))
+}
+
+/**
+ * Slides a window over string or array and also returns the remainder of the string.
+ * For example, if n is 3 and input is "hello",
+ * the function will return an array of substrings of length 3: [["hel", "lo"], ["ell", "o"], ["llo", ""]].
+ * @param n the window size
+ * @returns all scanned items
+ */
+export const slidingWithRest = (n: number) => (input: string): [string, string][] => {
+  if (input.length <= n) return [[input, ""]]
+  return [[input.slice(0, n), input.slice(n)] as [string, string]].concat(slidingWithRest(n)(input.slice(1)))
+}
+
+const recParse = (seq: string, groupSizes: number[], depth = 0): number => {
+  const cached = readFromCache(seq, groupSizes)
+  if (cached !== undefined) return cached
+
+  // We can ignore dots at the beginning, they don't influence the amount
+  if (seq[0] === springCharacter.operational) {
+    const res = recParse(seq.slice(1), groupSizes, depth + 1)
+    saveToCache(seq, groupSizes, res)
+    return res
+  }
+
+  // At this point we know we have either a # or ? on the first position
+
+  const [currentSizeGroup, ...restGroups] = groupSizes
+
+  if (currentSizeGroup === undefined) {
+    // We have no more groups to create, we can return 1 if the remainder of the string
+    // is all dots or question marks
+    return seq.split('').every(c => [springCharacter.operational, springCharacter.unknown].includes(c)) ? 1 : 0
+  }
+
+  // Create first group
+  // A group needs to be surrounded by dots/emtpy space
+
+  // We're creating a sliding window where we'll iterate over all the possible groups
+  // for the first group amount.
+  const slidingWindow = slidingWithRest(currentSizeGroup)(seq)
+  let foundFirstDamagedSpring = -1 // -1 is it hasn't been found yet, any other positive values indicate the index of the first damaged spring in the window
+  let totalAmount = 0
+
+  // We will keep exploring these windows until we get our first
+  // Already existing damaged spring to the left of us
+  for (const [window, rest] of slidingWindow) {
+    const lastChar = rest.at(0) || "" // If this is empty, it's the boundary of the string
+
+    if (foundFirstDamagedSpring === 0) break // We should stop looking if the first damaged character is to the left of our window, as we can't have any valid groups anymore
+    if (foundFirstDamagedSpring > 0) foundFirstDamagedSpring -= 1
+    if (foundFirstDamagedSpring === -1 && window.includes(springCharacter.damaged)) foundFirstDamagedSpring = window.indexOf(springCharacter.damaged)
+
+    if (lastChar === springCharacter.damaged) {
+      // We can't create a group with the current window
+      // Because it'd connect to the next damaged spring
+      // Meaning the group would end up being bigger
+      continue
+    }
+
+    // Now we know we have a damaged spring in the window & last char is a dot or question mark
+    // What's left is to see if we have enough question marks and damaged springs to create the group
+    const nQuestionMarks = countStr(window, springCharacter.unknown)
+    const nDamagedSprings = countStr(window, springCharacter.damaged)
+    // logInd('currentSizeGroup: ', currentSizeGroup)
+    if (nQuestionMarks + nDamagedSprings === currentSizeGroup) {
+      // We can create the group
+      const amnt = recParse(rest.slice(1), restGroups, depth + 1)
+      saveToCache(window + springCharacter.operational, [currentSizeGroup], 1)
+      saveToCache(rest.slice(1), restGroups, amnt)
+      totalAmount += amnt
+      continue
+    }
+  }
+
+  return totalAmount
+}
+
+export const solvePart2 = (input: string, withUnfolding: boolean = false) => {
+  const lines = parseInputLines(input)
+    .map(line => {
+      let [springs, ...groupsSesrialised] = line.split(/[ ,]/)
+      let groups = groupsSesrialised.map(g => Number(g))
+
+      if (withUnfolding) {
+        springs = Array.from({ length: 5 }).map(_ => springs).join("?")
+        groups = Array.from({ length: 5 }).map(_ => groups).flat()
+      }
+
+      return [springs, groups] as const
+    })
+
+  let count = 0
+
+  lines.forEach(([springs, groups]) => {
+    const amnt = recParse(springs, groups)
+    count += amnt
+  })
+
+  return count
 }
